b901aadc36b9ada150c5a4435437201f9dd7c6ce
Ryan Guill <ryanguill@gmail.com>
2017-11-30 23:06:30 -0600

    day1-1

--- day1/1_1.py
+++ day1/1_1.py
@@ -1,20 +1,30 @@
 import sys
 sys.path.append(sys.path[0] + "/..") #because python relative imports are weird...
 
-from lib.utils import read_input, begin_terminal_block, end_terminal_block, memoize
+from lib.utils import read_input, begin_terminal_block, end_terminal_block
 
 DAY = 1
 
-
-def test(a):
-	return a * 2;
-
-
-
 begin_terminal_block(DAY)
 
-for line in read_input(DAY):
-	line = line.strip('\n')
-	print(line)
+toSum = []
+lastChar = None
+firstChar = None
+for char in read_input(DAY).read():
+	#print(char)
+	if firstChar is None:
+		firstChar = char
+	if char == lastChar:
+		toSum.append(int(char))
+	lastChar = char
 
-end_terminal_block()	
+if char == firstChar:
+	toSum.append(int(char))
+
+print(toSum)
+print(sum(toSum))		
+
+# too low 993
+
+end_terminal_block()
+
