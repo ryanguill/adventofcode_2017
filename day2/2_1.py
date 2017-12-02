c91622d56f58b2a2c8bb1051e16a3b2605cf59f2
Ryan Guill <ryanguill@gmail.com>
2017-12-01 23:12:54 -0600

    day 2, parts 1 and 2

--- day2/2_1.py
+++ day2/2_1.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append(sys.path[0] + "/..") #because python relative imports are weird...
-
 from lib.utils import read_input, begin_terminal_block, end_terminal_block
 
 DAY = 2
@@ -8,7 +7,13 @@ DAY = 2
 begin_terminal_block(DAY)
 #==============================================================================
 
+total = 0
+for line in read_input(DAY):
+	line = line.strip('\n').split('\t')
+	line = list(map(int, line))
+	total += int(max(line))-int(min(line))
 
+print(total)
 
 #==============================================================================
 end_terminal_block()
--- day2/2_2.py
+++ day2/2_2.py
@@ -1,11 +1,24 @@
 import sys
 sys.path.append(sys.path[0] + "/..") #because python relative imports are weird...
-
 from lib.utils import read_input, begin_terminal_block, end_terminal_block
+from itertools import permutations
 
 DAY = 2
 
 begin_terminal_block(DAY)
+#==============================================================================
 
+total = 0
+for line in read_input(DAY):
+	line = line.strip('\n').split('\t')
+	line = list(map(int, line))
 
+	for a, b in permutations(line, 2):
+		if a % b == 0:
+			total += max((a,b)) // min((a,b))
+			break
+
+print(total)
+
+#==============================================================================
 end_terminal_block()
