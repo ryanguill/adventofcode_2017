d119b0ece707856604b4d0e6883414c8f0a20e12
Ryan Guill <ryanguill@gmail.com>
2017-12-05 08:47:36 -0600

    day 5 part 2

---
+++ day5/5_2.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append(sys.path[0] + "/..") #because python relative imports are weird...
+from lib.utils import read_input, begin_terminal_block, end_terminal_block
+
+DAY = 5
+
+begin_terminal_block(DAY)
+#==============================================================================
+
+instructions = []
+for line in read_input(DAY).readlines():
+	instructions.append(int(line.strip()))
+
+current_pos = 0
+current_iteration = 0
+
+while True:
+	current_iteration += 1
+	offset = instructions[current_pos]
+	next_pos = current_pos + offset
+	instructions[current_pos] += 1 if offset < 3 else -1
+	current_pos = next_pos
+
+	if current_pos > len(instructions) -1 or current_pos < 0:
+		break
+
+print(current_iteration)
+
+
+#==============================================================================
+end_terminal_block()
