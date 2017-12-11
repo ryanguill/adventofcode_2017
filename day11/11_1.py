a967514d7d9d9abc63bfc24c15bb9da54af96421
Ryan Guill <ryanguill@gmail.com>
2017-12-11 09:02:22 -0600

    day 11 part 1

---
+++ day11/11_1.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append(sys.path[0] + "/..") #because python relative imports are weird...
+from lib.utils import read_input, begin_terminal_block, end_terminal_block, hash_sha1
+from itertools import cycle
+
+DAY = 11
+
+begin_terminal_block(DAY)
+#==============================================================================
+
+
+
+def cube_distance(a, b):
+	return (abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])) / 2
+
+def move (pos, direction):
+	x, y, z = pos
+	if direction == 'n':
+		return (x, y + 1, z -1)
+	elif direction == 'ne':
+		return (x + 1, y, z - 1)
+	elif direction == 'se':
+		return (x + 1, y - 1, z)
+	elif direction == 's':
+		return (x, y - 1, z + 1)
+	elif direction == 'sw':
+		return (x - 1, y, z + 1)
+	elif direction == 'nw':
+		return (x - 1, y + 1, z)
+	else:
+		print('unknown direction', direction)
+		quit()
+
+
+origin = current_pos = (0, 0, 0)
+for direction in read_input(DAY).readline().split(','):
+	current_pos = move(current_pos, direction.strip())
+
+
+print(current_pos)
+print(origin)
+print(cube_distance(origin, current_pos))
+
+
+
+
+#==============================================================================
+end_terminal_block()
